362485231abc757c82db6e1e92065ad55e306ccf
Vitaly <109906595+brainlessless@users.noreply.github.com>
2023-03-22 18:26:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        sum(10,20);
+//    }
+//    public static void sum(int a, int b){
+//
+//        System.out.println("a + b = " + (a+b));
+//    }
+//    public static void main(String[] args) {
+//        sum(10,20);
+//    }
+
+
+//    public static void divide(int a, int b){
+//
+//        System.out.println("a / b = " + (a/b));
+//    }
+//    public static void main(String[] args) {
+//        divide(20,20);
+//    }
+
+//        public static void mult(int a, int b){
+//            System.out.println("a * b = " + (a*b));
+//        }
+//        public static void main(String[] args) {
+//            mult(20,20);
+//        }
+
+//    public static void diff(int a, int b){
+//        System.out.println("a - b = " + (a-b));
+//    }
+//    public static void main(String[] args) {
+//        diff(30,20);
+//    }
+
+    //массивы
+
+    int[] arr = {10, 20, 35, 47, 5};
+    int min = arr[0];
+        for(int j:arr)
+    {
+        if (j < min) {
+            min = j;
+        }
+
+    }
+        System.out.println(min);
+
+//        int[] arr = {10,20,35,47,5};
+//        int max = arr[0];
+//        for (int j: arr) {
+//            if (j > max) {
+//                max = j;
+//            }
+//
+//        }
+//        System.out.println(max);
+
+//        int [] arr = {10, 20, 30, 40, 56};
+//            double average = 0;
+//        if (arr.length > 0) {
+//            double sum = 0;
+//            for (int i = 0; i < arr.length; i++) {
+//                sum += arr[i];
+//            }
+//        average = sum / arr.length;
+//
+//        }
+//        System.out.println(average);
+
+
+//        int[] arr = {10,20,35,47,5};
+//        for (int a: arr) {
+//            System.out.println(a);
+//        }
+
+
+}
+
+
+}
+
